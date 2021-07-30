26273865c223fd83fdc71aedbd7cc1d7f84357ac
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-07-30 10:59:00 -0700

    improve params documentation

--- R/params.R
+++ R/params.R
@@ -1,9 +1,9 @@
 #' Parameter Descriptions for wsgcolr functions
 #'
-#' @param detection_event A tibble of detection path data returned from `detection_event()`containing columns 'transmitter_id', 'timestep', 'path', 'array', 'array_rkm'.
-#' @param detection_timestep A tibble of aggregated detection data returned from `detection_timestep()` containing columns 'transmitter_id' and 'timestep'. If `NULL`, argument is ignored.
-#' @param detection_complete A tibble of complete detection data returned from `detection_complete()` containing columns 'transmitter_id' and 'timestep'.
-#' @param detection_ratio A tibble of detection rtio data returned from `detection_ratio()` containing columns 'timestep', 'n' and 'prop'.
+#' @param detection_event A tibble of detection path data returned from `wsgcolr::detection_event()`containing columns 'transmitter_id', 'timestep', 'path', 'array', 'array_rkm'.
+#' @param detection_timestep A tibble of aggregated detection data returned from `wsgcolr::detection_timestep()` containing columns 'transmitter_id' and 'timestep'. If `NULL`, argument is ignored.
+#' @param detection_complete A tibble of complete detection data returned from `wsgcolr::detection_complete()` containing columns 'transmitter_id' and 'timestep'.
+#' @param detection_ratio A tibble of detection rtio data returned from `wsgcolr::detection_ratio()` containing columns 'timestep', 'n' and 'prop'.
 #' @param detection A tibble of raw detection data containing columns 'transmitter_id' and 'datetime_utc'.
 #' @param receiver_group A character string of the column containing receiver group (e.g., station, zone, array).
 #' @param receiver_group_rkm A character string of the column containing receiver group rkm.
